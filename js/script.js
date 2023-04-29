6dc8e58434f10c96ced1e2df060a4bd628fb042e
al.falimby@gmail.com <al.falimby@gmail.com>
2023-04-29 19:24:25 +0700

    lagi

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+
+
+document.querySelector('#menu').onclick = () => {
+  
+  navbarNav.classList.toggle('active');
+  
+};
+const Menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+  if(!Menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active')
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+      setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+      }, 10)
+    });
